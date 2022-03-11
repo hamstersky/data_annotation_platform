566fece839adad60cf9c374334401acdd7bb5bf9
Dominik Chomicki <domc@itu.dk>
2022-03-11 11:13:23 +0000

    clean up comments

--- data_annotation_platform/segments_data.py
+++ data_annotation_platform/segments_data.py
@@ -5,8 +5,6 @@ class SegmentsData():
         self.data = pd.read_pickle(source_path)
         # TODO: Make the input data already in a format where this will not be necessary
         self.data.reset_index(inplace=True)
-        # self.data.reset_index(inplace=True)
-        # self.data.reset_index(inplace=True)
         # Extract only relevant features
         self.data = self.data[['xs', 'ys', 'class', 'frame_in', 'frame_out']]
         # In the initial state all segments are correct
