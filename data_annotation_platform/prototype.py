109759916d147a56d21147ce0051746ac09a182e
Dominik Chomicki <domc@itu.dk>
2022-03-11 11:56:49 +0000

    organize prototype code for easier refactoring

--- data_annotation_platform/prototype.py
+++ data_annotation_platform/prototype.py
@@ -10,35 +10,19 @@ import pandas as pd
 import numpy as np
 from segments_data import SegmentsData
 
-# create a plot and style its properties
 cap_w = 640
 cap_h = 360
-p = figure(
-    tools='pan,box_zoom,wheel_zoom,tap,save,reset',
-    active_scroll='wheel_zoom',
-    active_tap='auto',
-    x_range=(0, cap_w),
-    y_range=(0, cap_h)
-)
-p.toolbar.logo = None
-p.grid.visible = False
-p.axis.visible = False
-p.outline_line_width = 0
 
 cap = cv2.VideoCapture('../videos/video.m4v')
-ret = True
 total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 data = pd.read_pickle('../data/broken_trajectories.pkl')
 data.reset_index(inplace=True)
 data = data[['xs', 'ys', 'class', 'frame_in', 'frame_out']]
 segments = SegmentsData('../data/segments.pkl')
 
-def update_frame(attr, old, new):
-    frame_nr = new
-    frame = get_frame(frame_nr)
-    img = get_image_from_frame(frame)
-    plot_image(img)
-    get_data_range(frame_nr)
+# ===============
+# Helpers
+# ===============
 
 def get_frame(frame_nr):
     cap.set(cv2.CAP_PROP_POS_FRAMES, frame_nr)
@@ -59,7 +43,7 @@ def get_image_from_frame(frame):
     return img_orig
 
 def get_data_range(frame_nr):
-    global l
+    global trajectories
     global segments_lines
     global segments
     global data
@@ -73,7 +57,7 @@ def get_data_range(frame_nr):
     default_lc = [colors[int(i)] for i in subset['class']]
 
     # Merge two dictionaries
-    l.data_source.data = {
+    trajectories.data_source.data = {
         **subset.to_dict(orient='list'),
         'id': subset.index.values,
         **{
@@ -101,84 +85,36 @@ def get_data_range(frame_nr):
 def plot_image(img):
     img_plot.data_source.data['image'] = [img]
 
-img_source = ColumnDataSource(data=dict(
-    image=[]
-))
+def clear_trajectories():
+    table_source.data['traj_id'] = []
+    update_selection([], [])
 
-img_plot = p.image_rgba(
-    source=img_source,
-    image='image',
-    x=0,
-    y=0,
-    dw=cap_w,
-    dh=cap_h,
-    level='image'
-)    
+def update_selection(old, new):
+    if new == []:
+        trajectories_source.selected.indices = []
+    else:
+        trajectories_source.selected.indices = old + new
 
-slider = Slider(start=1, end=total_frames, value=1, step=1)
-slider.on_change('value_throttled', update_frame)
+def update_stats():
+    total = segments.data.shape[0]
+    correct_count = segments.data[segments.data['correct']==True].shape[0]
+    incorrect_count = segments.data[segments.data['correct']==False].shape[0]
+    ratio = correct_count / total
+    return f'''Correct: {correct_count}
+    Incorrect: {incorrect_count}
+    Ratio: {ratio}
+    '''
 
-# Set up initial plot
-# TODO: This initial definition might not be needed? At least not the keys
-source = ColumnDataSource(data={
-    'xs': [],
-    'ys': [],
-    'line_color': [],
-    'line_alpha': [],
-    'line_width': [],
-    'line_dash': []
-})
+# ===============
+# Callbacks
+# ===============
 
-segments_source = ColumnDataSource(data={
-    'xs': [],
-    'ys': [],
-    'line_color': [],
-    'line_alpha': [],
-    'line_width': [],
-    'line_dash': []
-})
-
-l = p.multi_line(
-    source=source,
-    line_color='line_color',
-    line_alpha='line_alpha',
-    line_width='line_width',
-    line_dash='line_dash',
-    hover_line_width=2.0,
-    hover_line_alpha=1.0,
-    selection_line_width=3.0,
-    selection_line_alpha=1.0,
-    selection_line_color='red',
-    nonselection_line_width=2,
-    nonselection_line_alpha=0.8
-)
-
-segments_lines = p.multi_line(
-    source=segments_source,
-    line_color='line_color',
-    line_alpha='line_alpha',
-    line_width='line_width',
-    line_dash='line_dash',
-    hover_line_width=2.0,
-    hover_line_alpha=1.0,
-    selection_line_width=2.0,
-    selection_line_alpha=1.0,
-    # nonselection_line_width=0.8,
-    # nonselection_line_alpha=0.3
-)
-
-p.add_tools(HoverTool(
-    show_arrow=False,
-    line_policy='nearest',
-    renderers=[l, segments_lines],
-    tooltips=[("id", "@id"), ("frame_in", "@frame_in"), ("frame_out", "@frame_out")]
-))
-
-p.plot_height = 360
-p.plot_width = 640
-update_frame('value', 1, 1)
-
-
+def update_frame(attr, old, new):
+    frame_nr = new
+    frame = get_frame(frame_nr)
+    img = get_image_from_frame(frame)
+    plot_image(img)
+    get_data_range(frame_nr)
 
 lock = False
 def tap_handler(attr, old, new):
@@ -187,7 +123,7 @@ def tap_handler(attr, old, new):
     if not lock:
         lock = True
         if len(new) > 0:
-            traj_id = source.data['id'][new[0]]
+            traj_id = trajectories_source.data['id'][new[0]]
             table_source.stream(dict(traj_id=[traj_id]))
             update_selection(old, new)
         else:
@@ -199,19 +135,6 @@ def segments_tap_handler(attr, old, new):
     if len(new) > 0:
         traj_id = segments_source.data['id'][new[0]]
         table_source.stream(dict(traj_id=[traj_id]))
-
-# p.on_event(Tap, tap_handler)
-# l.data_source.on_change('tap', tap_handler)
-source.selected.on_change('indices', tap_handler)
-segments_source.selected.on_change('indices', segments_tap_handler)
-columns = [
-    TableColumn(field="traj_id", title="Trajectory id"),
-]
-table_source = ColumnDataSource(data=dict(traj_id=[]))
-table = DataTable(source=table_source, columns=columns)
-connect = Button(label='Connect')
-forward = Button(label='+30 frames')
-wrong = Button(label='Wrong')
 
 def connect_handler():
     global table_source
@@ -245,39 +168,146 @@ def wrong_handler():
     # TODO: Figure out if there's a better way to update the plot
     slider.trigger('value_throttled', 0, slider.value)
     clear_trajectories()
-    par.text = update_stats()
-    segments_table_source.data['correct'] = segments.data['correct']
-    
-def clear_trajectories():
-    table_source.data['traj_id'] = []
-    update_selection([], [])
+    stats.text = update_stats()
+    segments_table_source.data['correct'] = segments.data['correct']     
 
-def update_selection(old, new):
-    if new == []:
-        source.selected.indices = []
-    else:
-        source.selected.indices = old + new
+# ===============
+# Plot setup
+# ===============
 
-connect.on_click(connect_handler)
+def setup_plot():
+    p = figure(
+        tools='pan,box_zoom,wheel_zoom,tap,save,reset',
+        active_scroll='wheel_zoom',
+        active_tap='auto',
+        x_range=(0, cap_w),
+        y_range=(0, cap_h)
+    )
+    p.toolbar.logo = None
+    p.grid.visible = False
+    p.axis.visible = False
+    p.outline_line_width = 0
+    p.plot_height = 360
+    p.plot_width = 640
+    return p
+
+plot = setup_plot()
+
+def setup_sources():
+    img_source = ColumnDataSource(data=dict(
+        image=[]
+    ))
+
+    # TODO: This initial definition might not be needed? At least not the keys
+    # trajectories_source = ColumnDataSource(data={
+    #     'xs': [],
+    #     'ys': [],
+    #     'line_color': [],
+    #     'line_alpha': [],
+    #     'line_width': [],
+    #     'line_dash': []
+    # })
+    trajectories_source = ColumnDataSource()
+
+    segments_source = ColumnDataSource()
+    segments_source.selected.on_change('indices', segments_tap_handler)
+
+    table_source = ColumnDataSource(data=dict(traj_id=[]))
+    segments_table_source = ColumnDataSource(segments.data)
+    return (img_source, trajectories_source, segments_source, table_source, segments_table_source)
+
+img_source, trajectories_source, segments_source, table_source, segments_table_source = setup_sources()
+
+def setup_renderers():
+    img_plot = plot.image_rgba(
+        source=img_source,
+        image='image',
+        x=0,
+        y=0,
+        dw=cap_w,
+        dh=cap_h,
+        level='image'
+    )
+
+    trajectories = plot.multi_line(
+        source=trajectories_source,
+        line_color='line_color',
+        line_alpha='line_alpha',
+        line_width='line_width',
+        line_dash='line_dash',
+        hover_line_width=2.0,
+        hover_line_alpha=1.0,
+        selection_line_width=3.0,
+        selection_line_alpha=1.0,
+        selection_line_color='red',
+        nonselection_line_width=2,
+        nonselection_line_alpha=0.8
+    )
+
+    segments_lines = plot.multi_line(
+        source=segments_source,
+        line_color='line_color',
+        line_alpha='line_alpha',
+        line_width='line_width',
+        line_dash='line_dash',
+        hover_line_width=2.0,
+        hover_line_alpha=1.0,
+        selection_line_width=2.0,
+        selection_line_alpha=1.0,
+        # nonselection_line_width=0.8,
+        # nonselection_line_alpha=0.3
+    )
+
+    return (img_plot, trajectories, segments_lines)
+
+img_plot, trajectories, segments_lines = setup_renderers()
+
+plot.add_tools(HoverTool(
+    show_arrow=False,
+    line_policy='nearest',
+    renderers=[trajectories, segments_lines],
+    tooltips=[("id", "@id"), ("frame_in", "@frame_in"), ("frame_out", "@frame_out")]
+))   
+
+# Setup initial frame
+update_frame('value', 1, 1)
+
+# ===============
+# Widgets / Layout
+# ===============
+
+# Slider
+slider = Slider(start=1, end=total_frames, value=1, step=1)
+slider.on_change('value_throttled', update_frame)
+
+# Buttons 
+forward = Button(label='+30 frames')
 forward.on_click(forward_frames)
+
+connect = Button(label='Connect')
+connect.on_click(connect_handler)
+
+wrong = Button(label='Wrong')
 wrong.on_click(wrong_handler)
 
-def update_stats():
-    # total = segments.shape[0]
-    # correct_count = segments[segments['correct']==True].shape[0]
-    # incorrect_count = segments[segments['correct']==False].shape[0]
-    # ratio = correct_count / total
-    # return f'''Correct: {correct_count}
-    # Incorrect: {incorrect_count}
-    # Ratio: {ratio}
-    # '''
-    return ''
+# Stats
+stats = Paragraph(text=update_stats())
 
-par = Paragraph(text=update_stats())
+# Selection table
+selection_table_cols = [TableColumn(field="traj_id", title="Trajectory id")]
+selection_table = DataTable(source=table_source, columns=selection_table_cols)
 
-columns2 = [TableColumn(field=c, title=c) for c in segments.data.columns]
-segments_table_source = ColumnDataSource(segments.data)
-segments_table = DataTable(source=segments_table_source, columns=columns2)
+# Selection callbacks
+trajectories_source.selected.on_change('indices', tap_handler)
+# segments_source.selected.on_change('indices', segments_tap_handler)
 
-curdoc().add_root(layout([[p, [slider, forward, connect, wrong], table], [par], [segments_table]]))
-# curdoc().add_root(column(slider, forward, p, table, connect, wrong, par, segments_table))
+# Segments table
+segments_table_cols = [TableColumn(field=c, title=c) for c in segments.data.columns]
+segments_table = DataTable(source=segments_table_source, columns=segments_table_cols)
+
+# Create layout
+curdoc().add_root(layout([
+    [plot, [slider, forward, connect, wrong]],
+    [stats],
+    [selection_table, segments_table]
+]))
