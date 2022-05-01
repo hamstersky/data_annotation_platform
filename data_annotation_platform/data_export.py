6a3ff6576ba29ca02311116d287f444c04c81f32
Dominik Chomicki <domc@itu.dk>
2022-05-01 16:50:09 +0200

    add data export

---
+++ data_annotation_platform/data_export.py
@@ -0,0 +1,16 @@
+from bokeh.models import Button, ColumnDataSource, CustomJS
+import os
+import state
+
+
+def create_download_btn():
+    dl_button = Button(label="Download")
+    source = ColumnDataSource(state.segments.get_data())
+    dl_button.js_on_event(
+        "button_click",
+        CustomJS(
+            args=dict(source=source),
+            code=open(os.path.join(os.path.dirname(__file__), "download.js")).read(),
+        ),
+    )
+    return dl_button
