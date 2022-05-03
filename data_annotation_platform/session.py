0460a54bc2dc57a4ebd49e38659a0c627d94250a
Dominik Chomicki <domc@itu.dk>
2022-05-03 09:45:31 +0200

    fix bug with saving the progress

--- data_annotation_platform/session.py
+++ data_annotation_platform/session.py
@@ -1,31 +1,25 @@
-from bokeh.models import PreText, CustomJS, Button
+from bokeh.models import CustomJS, Button
 from bokeh.plotting import curdoc
 
-import settings
 import state
-import styles
 
 
 def save_progress():
-    # Bokeh doesn't provide an option to call arbitrary JS, it always requires a callback
-    # As a workaround, create a text widget which is not visible
-    # When the text changes, it trigger Custom JS code
-    store_cookie_trigger = PreText(text="", visible=False)
-
     store_cookie = CustomJS(
         args=dict(uid=state.uid),
         code="""
+            console.log('hi')
             document.cookie = "uid=" + uid + ";max-age=31536000;SameSite=Strict"
         """,
     )
-    store_cookie_trigger.js_on_change("text", store_cookie)
 
     def save():
-        # Trigger the callback
-        store_cookie_trigger.text = f"{store_cookie_trigger.text}1"
+        # Trigger JS script
+        save_btn.name = f"{save_btn.name}1"
         state.segments.export_data(f"./data/{state.uid}")
 
     save_btn = Button(label="Save progress", margin=(100, 0, 0, 0))
     save_btn.on_click(save)
+    save_btn.js_on_change("name", store_cookie)
     curdoc().add_periodic_callback(save, 60000)
     return save_btn
