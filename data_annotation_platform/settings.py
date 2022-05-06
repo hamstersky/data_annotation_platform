8ec2e5ef07954006c9dee02f66d2427cc849cd1c
Dominik Chomicki <domc@itu.dk>
2022-05-06 11:23:28 +0200

    remove print

--- data_annotation_platform/settings.py
+++ data_annotation_platform/settings.py
@@ -1,7 +1,6 @@
 import pathlib
 
 project_path = pathlib.Path(__file__).parent.absolute()
-print(project_path)
 segments_path = f"{project_path}/data/segments.pkl"
 trajectories_path = f"{project_path}/data/broken_trajectories.pkl"
 FRAME_INTERVAL = 1800
