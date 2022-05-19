68867c64b64e2c8578cbcc16c9813ae2f4c8cc08
Dominik Chomicki <domc@itu.dk>
2022-05-19 08:52:03 +0200

    documentation and refactoring for labeling controls

--- data_annotation_platform/ui/labeling.py
+++ data_annotation_platform/ui/labeling.py
@@ -4,52 +4,55 @@ from app.helpers import refresh_frame, clear_selected_data, update_buttons_state
 
 
 def create_labeling_controls():
+    """Returns a list of widgets used for labeling trajectories."""
+
+    # === CALLBACKS ===
     def handle_connect():
-        # Creates segments needed to connect the supplied trajectories (ids)
-        # Connections will be done in the order of the supplied ids
-        ids = state.trajectories.selected_ids
-        pairs = [tuple(map(int, x)) for x in zip(ids, ids[1:])]
-        new_frame = 0
-        for t1_ID, t2_ID in pairs:
-            segment = state.trajectories.connect(t1_ID, t2_ID)
-            # TODO: Consider the append being an internal call. Possibly still return the segment
-            state.segments.add_segment(segment)
-            new_frame = int(segment["frame_out"])
-            refresh_frame("", 0, new_frame)
+        """Callback for connecting the currently selected trajectories."""
+        t1Id, t2Id = state.trajectories.selected_ids
+        segment = state.trajectories.connect(t1Id, t2Id)
+        state.segments.add_segment(segment)
+        new_frame = int(segment["frame_out"])
+        # Jump to the the last frame of the new segment so the user can see the whole trajectory
+        refresh_frame("", 0, new_frame)
 
     def handle_reset_label():
+        """Callback for resetting the labels for all currently selected segments."""
         ids = []
-        # TODO: This could be replaced not to rely on the internals of segments if the currently selected ids are moved to state
-        for source in state.segments.views:
-            for i in source.selected.indices:
-                ids.append(source.data["id"][i])
+        for view in state.segments.views:
+            for i in view.selected.indices:
+                ids.append(view.data["id"][i])
         state.segments.update_label(label=None, comments="", ids=ids)
         refresh_frame("", 0, state.current_frame)
 
     def handle_label_btn_click(label):
+        """Callback generator for updating the label of the selected segment."""
+
         def callback():
             state.segments.update_label(label=label, comments=incorrect_comment.value)
             refresh_frame("", 0, state.current_frame)
 
         return callback
 
+    # === WIDGETS ===
+    # Tags are used for for distinguishing between controls relevant for broken trajectories and segments
     segments_labeling_tags = ["labeling", "segments-labeling"]
     trajectories_labeling_tags = ["labeling", "trajectories-labeling"]
     label_btn_settings = {"disabled": True}
+
+    # == Connect button ==
     connect_btn = Button(
         label="Connect", tags=trajectories_labeling_tags, **label_btn_settings
     )
     connect_btn.on_click(handle_connect)
 
-    # Restore segment button
-    reset_label_btn = Button(label="Reset label", name="reset-btn", visible=False)
-    reset_label_btn.on_click(handle_reset_label)
-
-    # Wrong connection component
+    # == Incorrect segment button ==
     incorrect_btn = Button(
         label="Incorrect segment", tags=segments_labeling_tags, **label_btn_settings
     )
     incorrect_btn.on_click(handle_label_btn_click(False))
+
+    # == Reasons for incorrect segment dropdown ==
     incorrect_options = [
         "lack of good connection",
         "large distance",
@@ -66,32 +69,32 @@ def create_labeling_controls():
         name="incorrect-comment",
     )
 
+    # == Correct segment button ==
     correct_btn = Button(
         label="Correct segment", tags=segments_labeling_tags, **label_btn_settings
     )
     correct_btn.on_click(handle_label_btn_click(True))
 
+    # == Reset label button ==
+    reset_label_btn = Button(label="Reset label", name="reset-btn", visible=False)
+    reset_label_btn.on_click(handle_reset_label)
+
+    # == Reset current selection button ==
     reset_select_btn = Button(label="Reset selection")
     reset_select_btn.on_click(clear_selected_data)
 
-    labeling_controls = [
-        reset_select_btn,
-        reset_label_btn,
-        connect_btn,
-        correct_btn,
-        incorrect_btn,
-        incorrect_comment,
-    ]
-
+    # Collect all buttons in a list so it can be used to easily add a callback to all of them
     BUTTONS = [
         reset_select_btn,
         reset_label_btn,
         connect_btn,
-        incorrect_btn,
         correct_btn,
+        incorrect_btn,
     ]
-
     for btn in BUTTONS:
         btn.on_click(update_buttons_state)
 
-    return labeling_controls
+    # The final controls are the buttons and the incorrect comment dropdown
+    controls = [*BUTTONS, incorrect_comment]
+
+    return controls
