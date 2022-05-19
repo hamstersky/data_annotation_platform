d56027c92d195dce286387d6c3e9ab9ec7ef3fc1
Dominik Chomicki <domc@itu.dk>
2022-05-19 09:25:14 +0200

    document and refactor navigation

--- data_annotation_platform/ui/navigation.py
+++ data_annotation_platform/ui/navigation.py
@@ -5,6 +5,8 @@ import ui.styles as styles
 
 
 def handle_frame_navigation(value):
+    """Callback generator for updating the current frame."""
+
     def callback():
         new_frame = state.current_frame + value
         handle_jump_to_frame("", 0, new_frame)
@@ -12,14 +14,20 @@ def handle_frame_navigation(value):
     return callback
 
 
-def next_interest_handler():
+def handle_next_interest():
+    """Callback for jumping to frame with the next point of interest."""
+
     frame_ins = state.segments.current_frame_view.data["frame_in"]
-    frame = max(frame_ins) if frame_ins else state.current_frame
-    next_frame = state.segments.get_next_interest_frame(int(frame))
+    # Find the segment with the latest frame_in in the current frame view
+    max_frame = int(max(frame_ins)) if frame_ins else state.current_frame
+    # Find the next not annotated segment with frame_in after max_frame
+    next_frame = state.segments.get_next_interest_frame(max_frame)
     handle_jump_to_frame("", 0, next_frame)
 
 
 def create_navigation():
+    """Returns a list of widgets used for navigating frames."""
+    # == Jump to input field ==
     jump_to = NumericInput(
         low=1,
         high=state.total_frames,
@@ -28,17 +36,24 @@ def create_navigation():
     )
     jump_to.on_change("value", handle_jump_to_frame)
 
-    btn_size = 75
+    # == Frame navigation buttons ==
+    # Define the type of frame navigation buttons in format {frames: label}
     labels = {-30: "-1s", -1: "-1 frame", 1: "+1 frame", 30: "+1s"}
     btns = []
     for frames, label in labels.items():
-        btn = Button(label=label, sizing_mode="fixed", height=btn_size, width=btn_size)
+        btn = Button(
+            label=label,
+            sizing_mode="fixed",
+            height=styles.NAV_BTN_SIZE,
+            width=styles.NAV_BTN_SIZE,
+        )
         btn.on_click(handle_frame_navigation(frames))
         btns.append(btn)
 
+    # == Next interest button ==
     next_interest = Button(
-        label="Jump to next interest", height=btn_size, width_policy="min"
+        label="Jump to next interest", height=styles.NAV_BTN_SIZE, width_policy="min"
     )
-    next_interest.on_click(next_interest_handler)
+    next_interest.on_click(handle_next_interest)
 
     return [jump_to, *btns, next_interest]
--- data_annotation_platform/ui/session.py
+++ data_annotation_platform/ui/session.py
@@ -6,21 +6,28 @@ import ui.state as state
 
 
 def save_progress():
+    """Returns button for saving progress and add automatic periodic save."""
+
+    # JavaScript callback to save a cookie in the user's browser
     store_cookie = CustomJS(
         args=dict(uid=state.uid),
         code="""
-            console.log('hi')
             document.cookie = "uid=" + uid + ";max-age=31536000;SameSite=Strict"
         """,
     )
 
     def save():
-        # Trigger JS script
         save_btn.name = f"{save_btn.name}1"
+        # Save the data on the server
         state.segments.export_data(f"{settings.project_path}/data/{state.uid}")
 
     save_btn = Button(label="Save progress", margin=(100, 0, 0, 0))
     save_btn.on_click(save)
+
+    # Bokeh doesn't allow for arbitrary JS exectution, it can only be done via a callback
+    # As a workaround, the button's name property will be updated to trigger the callback
     save_btn.js_on_change("name", store_cookie)
+
+    # Automatic saves every minute
     curdoc().add_periodic_callback(save, 60000)
     return save_btn
