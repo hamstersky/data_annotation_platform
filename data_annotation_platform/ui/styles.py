d012cfa96d728c7d6639e2e1716a85d494056b12
Dominik Chomicki <domc@itu.dk>
2022-05-19 09:56:13 +0200

    add comment to styles

--- data_annotation_platform/ui/styles.py
+++ data_annotation_platform/ui/styles.py
@@ -1,3 +1,6 @@
+# This file defines constants for styles that are used components of the UI
+# This allows to control some visual aspects of the application from one place
+
 NAV_BTN_HEIGHT = 75
 PLOT_WIDTH = 1280
 PLOT_HEIGHT = 720
