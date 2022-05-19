23ce59019609507ef262b799585ae26dabea15ab
Dominik Chomicki <domc@itu.dk>
2022-05-19 12:16:11 +0200

    document and commit trajectory plot

--- data_annotation_platform/ui/trajectory_plot.py
+++ data_annotation_platform/ui/trajectory_plot.py
@@ -4,17 +4,26 @@ import ui.styles as styles
 
 
 class TrajectoryPlot:
-    def __init__(self, trajectories_data, segments_data):
-        # TODO: Pass as a paremeter
+    """
+    A class to represent the plot with trajectories. It set ups the plot with all the necessary settings.
+
+    Attributes:
+        trajectories: a DataFrame with broken trajectories data
+        segments: a DataFrame with segments data
+    """
+
+    def __init__(self, trajectories, segments):
         self.cap_w = 640
         self.cap_h = 360
         self.img_source = ColumnDataSource(data=dict(image=[]))
         self.setup_plot()
-        self.setup_renderers(trajectories_data, segments_data)
+        self.setup_renderers(trajectories, segments)
         self.setup_legend()
         self.setup_tools()
 
     def setup_plot(self):
+        """Creates a plot object and saves it as an instance variable."""
+
         p = figure(
             tools="pan,wheel_zoom,tap,reset",
             toolbar_location="below",
@@ -32,7 +41,9 @@ class TrajectoryPlot:
         p.plot_width = styles.PLOT_WIDTH
         self.plot = p
 
-    def setup_renderers(self, trajectories_data, segments_data):
+    def setup_renderers(self, trajectories, segments):
+        """Adds renderers to the plot."""
+
         self.img_plot = self.plot.image_rgba(
             source=self.img_source,
             image="image",
@@ -44,7 +55,7 @@ class TrajectoryPlot:
         )
 
         self.trajectories_lines = self.plot.multi_line(
-            source=trajectories_data.current_frame_view,
+            source=trajectories.current_frame_view,
             line_color="line_color",
             line_alpha=0.8,
             line_width=2.0,
@@ -53,14 +64,13 @@ class TrajectoryPlot:
             hover_line_alpha=1.0,
             selection_line_width=4.0,
             selection_line_alpha=1.0,
-            # selection_line_color='red',
             nonselection_line_width=2.0,
             nonselection_line_alpha=0.7,
             legend_label="broken trajectories",
         )
 
         self.segments_lines = self.plot.multi_line(
-            source=segments_data.current_frame_view,
+            source=segments.current_frame_view,
             line_color="line_color",
             line_alpha=0.8,
             line_width=2.0,
@@ -68,7 +78,6 @@ class TrajectoryPlot:
             hover_line_width=2.0,
             hover_line_alpha=1.0,
             selection_line_width=4.0,
-            # selection_line_color='black',
             selection_line_alpha=1.0,
             nonselection_line_width=2.0,
             nonselection_line_alpha=0.7,
@@ -92,7 +101,17 @@ class TrajectoryPlot:
             legend_label="correct segments",
         )
 
+        self.candidates = self.plot.multi_line(
+            line_color="brown",
+            line_alpha=0.8,
+            line_width=2.0,
+            line_dash="solid",
+            legend_label="candidate trajectories",
+        )
+
     def setup_tools(self):
+        """Adds a hover tool to the plot."""
+
         self.plot.add_tools(
             HoverTool(
                 show_arrow=False,
@@ -107,9 +126,13 @@ class TrajectoryPlot:
         )
 
     def setup_legend(self):
+        """Configures the legend for the plot."""
         self.plot.legend.orientation = "horizontal"
         self.plot.legend.spacing = 15
-        self.plot.legend.background_fill_alpha = 0.3
+        self.plot.legend.background_fill_alpha = 0.6
+        self.plot.legend.border_line_dash = "solid"
+        self.plot.legend.border_line_color = "black"
 
     def update_img(self, img):
+        """Helper method for updating the frame image in the plot."""
         self.img_plot.data_source.data["image"] = [img]
