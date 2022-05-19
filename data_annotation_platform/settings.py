8ffe531596523077599ebcc37a81b1dc79f40a04
Dominik Chomicki <domc@itu.dk>
2022-05-19 12:30:23 +0200

    document settings

--- data_annotation_platform/settings.py
+++ data_annotation_platform/settings.py
@@ -1,5 +1,8 @@
 import pathlib
 
+# This file defines settings for the application
+# This allows to control settings that apply to the entire application from one place
+
 project_path = pathlib.Path(__file__).parent.absolute()
 segments_path = f"{project_path}/data/segments.pkl"
 trajectories_path = f"{project_path}/data/broken_trajectories.pkl"
